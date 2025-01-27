42a6d6c5bef3fd62d3efaa70edb7ef30510e696e
Karen020701 <karenchicaiza989@gmail.com>
2025-01-27 00:55:24 -0500

    Bug fix

--- backend/Product/deleteProduct/src/main/java/com/deleteProduct/deleteProduct/service/deleteProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.deleteProduct.deleteProduct.service;
-
-import com.deleteProduct.deleteProduct.models.deleteProductModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface deleteProductRepository extends JpaRepository<deleteProductModel, Long>{
-    
-}
