c9bbb7c70b2a5bc71c59494e1f7e72b549c6ff8e
Alejandra Mishell Calvache Ortiz <amcalvacheo@uce.edu.ec>
2025-02-12 15:39:37 -0500

    fix: :bug: bug fixes

--- backend/Inventories/create-inventories/utils/validation.py
+++ backend/Inventories/create-inventories/utils/validation.py
@@ -8,6 +8,6 @@ def validate_inventory_data(data):
         return False, {"error": "Invalid amount"}, 400
     
     if not isinstance(data["price"], (int, float)) or data["price"] <= 0:
-        return False, {"error": "Precio inválido"}, 400
+        return False, {"error": "Invalid price"}, 400
     
     return True, None, None
