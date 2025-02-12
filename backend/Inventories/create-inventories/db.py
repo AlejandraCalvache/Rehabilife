e99662e61d3c93d24291ea1c32a592004c754595
Alejandra Mishell Calvache Ortiz <amcalvacheo@uce.edu.ec>
2025-02-12 10:16:34 -0500

    fix: :art: Changes in the code db.py

--- backend/Inventories/create-inventories/db.py
+++ backend/Inventories/create-inventories/db.py
@@ -8,6 +8,9 @@ db = SQLAlchemy()
 
 def init_db(app):
     """Inicializa la base de datos con la configuración de la app."""
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@"
+        f"{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DB')}"
+    )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
