6fee2acdc030c585086365896c064eaa4666b36e
Alejandra Mishell Calvache Ortiz <amcalvacheo@uce.edu.ec>
2025-02-12 16:01:26 -0500

    docs: :memo: Update comments

--- backend/Inventories/create-inventories/services/inventory_service.py
+++ backend/Inventories/create-inventories/services/inventory_service.py
@@ -1,7 +1,7 @@
 from models.inventory_model import db, Inventory
 
 def add_inventory_item(name, description, quantity, price):
-    """Add a new product to the inventory."""
+    """Add a new product to the inventory"""
     new_item = Inventory(name=name, description=description, quantity=quantity, price=price)
     db.session.add(new_item)
     db.session.commit()
