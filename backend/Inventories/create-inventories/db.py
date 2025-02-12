3044bbe3273f0cafb742808615dde9931138b644
Alejandra Mishell Calvache Ortiz <amcalvacheo@uce.edu.ec>
2025-02-12 13:02:41 -0500

    fix: :bug: bug fixes

--- backend/Inventories/create-inventories/db.py
+++ backend/Inventories/create-inventories/db.py
@@ -7,7 +7,7 @@ load_dotenv()
 db = SQLAlchemy()
 
 def init_db(app):
-    """Inicializar la base de datos con la configuración de la aplicación."""
+    """Initialize the database with the application configuration."""
     app.config["SQLALCHEMY_DATABASE_URI"] = (
         f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@"
         f"{os.getenv('DATABASE_URL')}/{os.getenv('MYSQL_DB')}"
